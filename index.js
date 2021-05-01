3a56bdb08e50ff2c1fc8b94daefc77c62d488dbe
Imran Ahmed <96imranahmed@gmail.com>
2021-05-02 02:11:17 +0600

    Fixes

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const googlePlayURI = "https://play.google.com/store/apps/details?id=com.pathao.user"
 const iosURI = "https://itunes.apple.com/us/app/pathao/id1201700952"
 
-$(document).ready(function() {
-    const url = new URL(window.location.href);
-    if (url.searchParams.has('type')) {
-        let cType = url.searchParams.get('type')
-        url.searchParams.delete('type')
-        if (cType == 'install') {
-            appInstallRedirect(url)
-        } else if (cType == 'campaign') {
-            campaignRedirect(url)
-        }
+const url = new URL(window.location.href);
+if (url.searchParams.has('type')) {
+    let cType = url.searchParams.get('type')
+    url.searchParams.delete('type')
+    if (cType == 'install') {
+        appInstallRedirect(url)
+    } else if (cType == 'campaign') {
+        campaignRedirect(url)
     }
-});
+}
 
 function appInstallRedirect(url) {
     setTimeout(function() {
@@ -26,7 +24,7 @@ function appInstallRedirect(url) {
         }
         let url_out = new URL(base_url)
         url.searchParams.delete('target');
-        url_out.search = url.searchParams
+        url_out.searchParams.appendAll(url.searchParams)
         window.location.replace(url_out.toString());
     }, 10);
 }
